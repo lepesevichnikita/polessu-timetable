1f78c0f7043d1a1eeec08e5cb5f982fda09753de
Nikita <lepesevich.nikita@yandex.ru>
2018-10-27 01:33:36 +0300

    add logging

--- app/workers/records_creating_worker.rb
+++ app/workers/records_creating_worker.rb
@@ -3,6 +3,7 @@ class RecordsCreatingWorker
 
   def perform(records, records_type)
     records_class = TimetableReader::REQUIRED_TYPES[records_type]
+    logger.info "#{ records_type }:#{ records_class }:#{ records }\n"
     records_class.send(:first_or_create!, records) if !records_class.nil?
   end
 end
