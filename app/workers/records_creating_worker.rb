ebdfff935eb9585681a7eac116e54aaa8fb59e0f
Nikita <lepesevich.nikita@yandex.ru>
2018-10-27 01:28:17 +0300

    fix

--- app/workers/records_creating_worker.rb
+++ app/workers/records_creating_worker.rb
@@ -2,7 +2,7 @@ class RecordsCreatingWorker
   include Sidekiq::Worker
 
   def perform(records, records_type)
-    records_class = TimetableReader::REQUIRED_TYPES[record_type]
+    records_class = TimetableReader::REQUIRED_TYPES[records_type]
     records_class.send(:first_or_create!, records) if !records_class.nil?
   end
 end
