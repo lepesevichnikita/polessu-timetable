b9fa73ea84fec1fdc252e3d89a2f9b24abcb3ce5
Nikita <lepesevich.nikita@yandex.ru>
2018-10-27 01:06:04 +0300

    add sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
+require 'sidekiq/web'
+mount Sidekiq::Web => '/sidekiq'
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
