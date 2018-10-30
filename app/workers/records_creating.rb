ac1844981ed0db5cc433bada4a867c06e0427984
Nikita <lepesevich.nikita@yandex.ru>
2018-10-30 03:33:41 +0300

    fix

--- app/workers/records_creating.rb
+++
@@ -1,7 +0,0 @@
-class RecordsCreatingWorker
-  include Sidekiq::Worker
-
-  def perform(items_of_required_type, required_type)
-    Timetable::create_records items_of_required_type, required_type
-  end
-end
