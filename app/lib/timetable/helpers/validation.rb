97756c9ccb8f85a09c7b162581ec93779075b76c
Nikita <lepesevich.nikita@yandex.ru>
2018-11-04 02:09:54 +0300

    remove self. from get_timetable_from

--- app/lib/timetable/helpers/validation.rb
+++ app/lib/timetable/helpers/validation.rb
@@ -2,20 +2,26 @@ module Timetable
   module Helper
     # This module provide a validation functional
     module Validation
-      def self.validate_attr_in_obj(key, obj)
+      module_function
+
+      def validate_attr_in_obj(key, obj)
         raise "#{key} not stored in #{obj}" until obj[key]
       end
 
-      def self.validate(value)
+      def validate(value)
         raise 'Value is nil' until value
       end
 
-      def self.should_be_instance_of(attr_name, value, class_object)
+      def should_be_instance_of(attr_name, value, class_object)
         should_be_instance_of(:class_name, class_object, Class)
         unless value.is_a?(class_object)
+          raise invalid_instance_message(
+            attr_name, class_object.name
+          )
+        end
       end
 
-      def self.invalid_instance_message(attr_name, class_name)
+      def invalid_instance_message(attr_name, class_name)
         "#{attr_name} should be an instance of #{class_name}"
       end
     end
