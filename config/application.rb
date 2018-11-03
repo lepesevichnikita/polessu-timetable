881f7b8a882227c439527d7226837b9ec7af3b52
Nikita <lepesevich.nikita@yandex.ru>
2018-11-04 00:47:09 +0300

    fix eager loading

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,8 @@ module PolessuTimetable
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.eager_load_paths << Rails.root.join('lib')
+    config.eager_load_paths << Rails.root.join('lib/timetable')
     config.active_job.queue_adapter = :sidekiq
   end
 end
