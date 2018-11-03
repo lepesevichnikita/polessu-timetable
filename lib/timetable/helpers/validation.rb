23bb3946ef3c2ea62dad2deab7fe1ca871271b83
Nikita <lepesevich.nikita@yandex.ru>
2018-11-04 01:40:05 +0300

    fix

---
+++ lib/timetable/helpers/validation.rb
@@ -0,0 +1,23 @@
+module Timetable
+  module Helper
+    # This module provide a validation functional
+    module Validation
+      def self.validate_attr_in_obj(key, obj)
+        raise "#{key} not stored in #{obj}" until obj[key]
+      end
+
+      def self.validate(value)
+        raise 'Value is nil' until value
+      end
+
+      def self.should_be_instance_of(attr_name, value, class_object)
+        should_be_instance_of(:class_name, class_object, Class)
+        raise invalid_instance_message(attr_name, class_object.name) unless value.is_a?(class_object)
+      end
+
+      def self.invalid_instance_message(attr_name, class_name)
+        "#{attr_name} should be an instance of #{class_name}"
+      end
+    end
+  end
+end
