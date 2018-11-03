2b0f7f594a3de61bda0dc41abd7f39593df1ed94
Nikita <lepesevich.nikita@yandex.ru>
2018-11-04 01:45:28 +0300

    fix

--- app/repositories/timetable_mongoid_repository.rb
+++ app/repositories/timetable_mongoid_repository.rb
@@ -1,5 +1,5 @@
 # This class provide a functional for timetable manipulating through mongoid models
+require 'timetable'
 class TimetableMongoidRepository < Timetable::Repository::Base
   extend Timetable::Repository::Mongoid
-
 end
