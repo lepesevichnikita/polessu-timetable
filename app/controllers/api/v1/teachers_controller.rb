3a4ea6bca2b21066f5e62b2a6d7f0669d6dc1c96
Nikita <lepesevich.nikita@yandex.ru>
2018-11-09 19:07:51 +0300

    add teacher controller under api/v1 namespace

---
+++ app/controllers/api/v1/teachers_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::TeachersController < Api::ApplicationController
+  before_action :set_teachers, only: %i[index search]
+  before_action :set_teacher, only: :show
+
+  private
+
+  def set_teachers
+    @teachers = Teacher.all
+    @teachers = @teachers.full_text_search(params[:query]) if params[:query]
+  end
+
+  def set_teacher
+    @teacher = Teacher.find(params[:id])
+  end
+
+end
