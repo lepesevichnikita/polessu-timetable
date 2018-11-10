cb1dbf5f2641673ce0e7608ab98e771bc0bd6fdd
Nikita <lepesevich.nikita@yandex.ru>
2018-11-10 03:26:28 +0300

    fix

--- app/models/part.rb
+++ app/models/part.rb
@@ -10,6 +10,5 @@ class Part
   belongs_to :group, foreign_key: :classid, index: true
   has_and_belongs_to_many :students, foreign_key: :studentids, index: true
 
-  setter_alias :studentids, :student_ids
-
 end
+
