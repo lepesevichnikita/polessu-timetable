abca8e07546dc064fa56f25ac180023cc40c5e4e
Nikita <lepesevich.nikita@yandex.ru>
2018-12-06 10:50:00 +0300

    Add alias
    Signed-off-by: Nikita <lepesevich.nikita@yandex.ru>

--- app/models/period.rb
+++ app/models/period.rb
@@ -2,12 +2,13 @@ class Period
   include Mongoid::Document
 
   field :name, type: String
-  field :period, type: String
+  field :period, type: String, as: :number
   field :short, type: String
   field :starttime, type: String
   field :endtime, type: String
 
   has_many :cards, foreign_key: :period, primary_key: :period
+
 
   validates_uniqueness_of :period
   validates_uniqueness_of :short
