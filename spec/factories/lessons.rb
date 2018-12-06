be794a99ecb9949e4b62848e394fccd5ab67ef1a
Nikita <lepesevich.nikita@yandex.ru>
2018-12-06 10:43:12 +0300

    Add comments Add params extracting
    Signed-off-by: Nikita <lepesevich.nikita@yandex.ru>

--- spec/factories/lessons.rb
+++ spec/factories/lessons.rb
@@ -7,29 +7,24 @@ FactoryBot.define do
     before :create do |lesson|
       days_num = Faker::Number.between(1, 6)
       weeks_num = Faker::Number.between(1, 20)
-      params = {
+      groups = create_list(:group, 5)
+      parts = groups.map(&:parts).flatten
+      teachers = groups.map(&:teacher).flatten
+      attributes = {
+          groups: groups,
+          parts: parts,
+          teachers: teachers,
+          classrooms: create_list(:classroom, 3),
           daysdef: first_or_create(Daysdef, short: days_num),
           weeksdef: first_or_create(Weeksdef, short: weeks_num),
           termsdef: first_or_create(Termsdef)
       }
-      lesson.set(params)
+      lesson.set(attributes)
     end
 
     periodspercard { Faker::Number.decimal(1, 1) }
     periodsperweek { Faker::Number.decimal(1, 1) }
     seminargroup { '*' }
 
-    after :create do |lesson|
-      groups = create_list(:group, 5)
-      parts = groups.map(&:parts).flatten
-      attributes = {
-        groups: groups,
-        parts: parts,
-        teachers: create_list(:teacher, 3),
-        classrooms: create_list(:classroom, 3)
-      }
-      lesson.update_attributes(attributes)
-      lesson
-    end
   end
 end
